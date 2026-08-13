f6da5886c11467596917a16b3d6193f731b4d623
mayana147 <Dennischimaobi912@gmail.com>
2026-08-13 12:01:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const DATA_FILE = path.join(__dirname, "data.json");
 app.use(express.json());
 app.use(express.static(__dirname));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "Index.html"));
+});
+
 
 // ==========================================
 // DATA
